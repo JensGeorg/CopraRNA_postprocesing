5b5b64f1ac57340697cfc39d55f9608ed2b5357b
JensGeorg <33146097+JensGeorg@users.noreply.github.com>
2018-03-26 16:42:21 +0200

    Update CopraRNA_postfiltering.r

--- CopraRNA_postfiltering.r
+++ CopraRNA_postfiltering.r
@@ -38,7 +38,7 @@ for(i in 1:length(dat)){
 
 for(i in 1:length(dat)){
 	nam<-paste(nam1[i],"_filtered.csv",sep="")
-	write.table( dat[[i]],file=nam ,sep=",", col.names=F)
+	write.table( dat[[i]],file=nam ,sep=",", row.names=F)
 
 
 }
